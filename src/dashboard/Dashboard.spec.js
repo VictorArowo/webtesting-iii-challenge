ec8499e82d8a6482e2336583725606ae0e4c1bcc
Victor Arowo <arowovici@gmail.com>
2019-10-14 15:11:03 +0100

    Added Testing for Components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,43 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+afterEach(rtl.cleanup);
+
+describe('Dashboard component', () => {
+  test('matches the snapshot!', () => {
+    expect(wrapper.container).toMatchSnapshot();
+  });
+
+  it('unlocked when component first mounts', () => {
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/\blocked\b/i)).not.toBeInTheDocument();
+  });
+
+  it('open when component first mounts', () => {
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/closed/i)).not.toBeInTheDocument();
+  });
+
+  it('gate can not be opened or closed when locked ', () => {
+    rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+    rtl.fireEvent.click(wrapper.queryByText(/lock gate/i));
+
+    expect(wrapper.queryByText(/open gate/i)).toBeDisabled();
+    expect(wrapper.queryByText(/close gate/i)).not.toBeInTheDocument();
+    expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+  });
+  it('lock gate is disabled when gate is open', () => {
+    rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+    rtl.fireEvent.click(wrapper.queryByText(/open gate/i));
+
+    expect(wrapper.queryByText(/lock gate/i)).toBeDisabled();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,35 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Display closed={true} locked={true} />);
+});
+
+afterEach(rtl.cleanup);
+
+describe('Display component', () => {
+  it('matches the snapshot!', () => {
+    expect(wrapper.container).toMatchSnapshot();
+  });
+
+  it('displays closed if closed prop is true', () => {
+    expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+  });
+
+  it('displays locked if locked prop is true', () => {
+    expect(wrapper.queryByText(/\blocked\b/i)).toBeInTheDocument();
+  });
+  it('displays open if closed prop is false', () => {
+    wrapper = rtl.render(<Display closed={false} locked={false} />);
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+  });
+
+  it('displays unlocked if locked prop is false', () => {
+    wrapper = rtl.render(<Display closed={false} locked={false} />);
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+  });
+});
